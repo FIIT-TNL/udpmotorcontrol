6c97c283fbe4893a834c615de4ec9b068e0cf75b
Martin Habovštiak <martin.habovstiak@gmail.com>
2015-02-28 12:07:23 +0100

    Deduplicated code and improved error reporting

--- motorcontrol.c
+++ motorcontrol.c
@@ -6,14 +6,34 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define SC_CHK(SYSCALL, ...) if(SYSCALL(__VA_ARGS__) < 0) { perror(#SYSCALL); return 1; }
+#define SC_CHK(SYSCALL, ...) sc_chk(SYSCALL(__VA_ARGS__), #SYSCALL, __FILE__, __LINE__)
+
+int sc_chk(int ret, const char *name, const char *filename, unsigned int line) {
+	if(ret < 0) {
+		fprintf(stderr, "Error in %s on line %u: ", filename, line);
+		perror(name);
+		exit(1);
+	}
+	return ret;
+}
 
 void setdir(int dir, int m1, int m2) {
 	char c = '0' + dir;
 	write(m1, &c, 1);
 	c = '0' + !dir;
 	write(m2, &c, 1);
+}
+
+int openGPIO(const char *number) {
+	static char path[256] = "/sys/devices/virtual/gpio/gpio";
+	size_t pinlen = strlen(number);
+
+	strcpy(path + 30, number);
+	strcpy(path + 30 + pinlen, "/value");
+
+	return open(path, O_WRONLY);
 }
 
 int main(int argc, char **argv) {
@@ -24,35 +44,14 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	char path[256] = "/sys/devices/virtual/gpio/gpio";
-	size_t pinlen = strlen(argv[1]);
-	strcpy(path + 30, argv[1]);
-	strcpy(path + 30 + pinlen, "/value");
-	int mA0 = open(path, O_WRONLY);
+	int mA0 = SC_CHK(openGPIO, argv[1]);
+	int mA1 = SC_CHK(openGPIO, argv[2]);
+	int mB0 = SC_CHK(openGPIO, argv[3]);
+	int mB1 = SC_CHK(openGPIO, argv[4]);
 	
-	pinlen = strlen(argv[2]);
-	strcpy(path + 30, argv[2]);
-	strcpy(path + 30 + pinlen, "/value");
-	int mA1 = open(path, O_WRONLY);
-	
-	pinlen = strlen(argv[3]);
-	strcpy(path + 30, argv[3]);
-	strcpy(path + 30 + pinlen, "/value");
-	int mB0 = open(path, O_WRONLY);
-	
-	pinlen = strlen(argv[4]);
-	strcpy(path + 30, argv[4]);
-	strcpy(path + 30 + pinlen, "/value");
-	int mB1 = open(path, O_WRONLY);
-	
-
 	if(argc > 5) sscanf(argv[5], "%hu", &port);
 
-	int sfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(sfd < 0) {
-		perror("socket");
-		return 1;
-	}
+	int sfd = SC_CHK(socket, AF_INET, SOCK_DGRAM, 0);
 
 	struct sockaddr_in saddr;
 	saddr.sin_family = AF_INET;
@@ -69,7 +68,7 @@ int main(int argc, char **argv) {
 	while(1) {
 		slen = sizeof(saddr);
 		SC_CHK(recvfrom, sfd, buf, 3, 0, (struct sockaddr *)&saddr, &slen);
-		sendto(sfd, buf + 2, 1, 0, (struct sockaddr *)&saddr, slen);
+		SC_CHK(sendto, sfd, buf + 2, 1, 0, (struct sockaddr *)&saddr, slen);
 		int left = buf[0];
 		int right = buf[1];
 
